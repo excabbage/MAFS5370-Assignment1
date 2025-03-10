62405b1b580c0e8ee55741c709df1674e98ef2f1
excabbage <zlijv@connect.ust.hk>
2025-03-10 21:29:33 +0800

    Update Test.py

--- PART II/Test.py
+++ PART II/Test.py
@@ -1,1 +1,22 @@
+####Unit Testing
+'''
 
+'''
+
+
+
+
+
+
+
+
+'''
+Integration Testing: test the TD0 working well.
+I use normal distribution of risky return to test the output policy.
+As referred in text book 'Fouundatoins of Reinforcement Learning with Applications in Finance', 
+the policy is independent to wealth:
+    xt = (mu - r)/( sigma^2 * 100 * (1+r)^(T-t-1) ) 
+which are all closed to 0
+From the test we can see the median of the xt with respect to wealth are close to zero, and 3/4 quantile are about 10%
+'''
+            
