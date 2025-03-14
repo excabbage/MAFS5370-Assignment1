db3490d899fee34aa7f5751d905d571413e00f76
excabbage <zlijv@connect.ust.hk>
2025-03-14 15:05:32 +0800

    Update Test.py

--- PART I/Test.py
+++ PART I/Test.py
@@ -12,7 +12,7 @@ Test State_Action:
 9.test initial funcion with assign parameter: initial the parameter as time=4,wealth=25. If print(test.time,test.wealth,test.action), result should be 4,25,50 respectively
 10.test hash function again: The hash value should be wealth * 11 + time = 279
 11.test hash_a function again: The hash_a value should be 279 * 101 + action = 28229
-12.test is_end function again: Test using Terminal time as 4,10,0 respectively, result should be True,False,True v.
+12.test is_end function again: Test using Terminal time as 4,10,0 respectively, result should be True,False,True.
 '''
 #1:result should be 0,100,50 respectively
 test = State_Action()
@@ -58,11 +58,11 @@ print(test.is_end(4),test.is_end(10),test.is_end(0))
 
 '''
 Test environment
-1.test initial funcion: If print(test.a,test.b,test.p,test.r,test.T), result should be 0.06,0.04,0.6,0.05,10 repsectively
+1.test initial function: If print(test.a,test.b,test.p,test.r,test.T), result should be 0.06,0.04,0.6,0.05,10 respectively
 2.test Y function: If run Y function 1000 timss and storage in the 'result' variable, 'reslut' shoulg consist of roughly 600 times of value 0.06, roughly 400 times of value 0.04. And there is no other value.
 3.test next_state function: If run next_state function 1000 times with imput parameter State_Action(4,250). The result should be 60% probability of State_Action(5,264), 40% probability of State_Action(5,261).
 '''
-#1:result should be 0.06,0.04,0.6,0.05,10 repsectively
+#1:result should be 0.06,0.04,0.6,0.05,10 respectively
 test = environment()
 print(test.a,test.b,test.p,test.r,test.T)
 
